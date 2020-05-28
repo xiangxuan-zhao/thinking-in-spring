b6f2028c618f16a56b570d40ef9a1e93040180e9
xiangxuan <zhaoxiangxuan@123drm.com>
2020-05-29 01:04:56 +0800

    BeanDefinition

---
+++ spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanDefinitionCreationDemo.java
@@ -0,0 +1,37 @@
+package org.geekbang.thinking.in.spring.bean.definition;
+
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+
+/**
+ * {@link org.springframework.beans.factory.config.BeanDefinition} 构建示例
+ * @author xiangxuan
+ * @date 2020/5/28
+ */
+public class BeanDefinitionCreationDemo {
+    public static void main(String[] args) {
+        //1.通过 BeanDefinitionBuilder 构建
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        //属性设置
+        beanDefinitionBuilder.addPropertyValue("id",11);
+        beanDefinitionBuilder.addPropertyValue("name","小熊");
+        //获取BeanDefinition实例
+        BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+        //BeanDefinition 不是终态，可以修改
+
+        //2.通过AbstractBeanDefinition 以及派生类
+        GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
+        //
+        genericBeanDefinition.setBeanClass(User.class);
+
+        MutablePropertyValues propertyValues = new MutablePropertyValues();
+        propertyValues.add("id",11)
+        .add("name","小熊");
+
+        genericBeanDefinition.setPropertyValues(propertyValues);
+    }
+
+}
